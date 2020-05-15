47e146b3ead1b403c43ffecf31b55e8c8622f7ad
Nikola Cvitaš <nikola.cvitas@btb.hr>
2020-05-15 14:57:44 +0200

    Initial commit

---
+++ src/test/java/hr/koios/projecttool/ProjecttoolApplicationTests.java
@@ -0,0 +1,13 @@
+package hr.koios.projecttool;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class ProjecttoolApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
