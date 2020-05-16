751f8c6a0e296a494ec2c0cc50e4357a2f453802
Nikola Cvitaš <nikola.cvitas@btb.hr>
2020-05-16 23:25:12 +0200

    React build

---
+++ src/main/resources/static/precache-manifest.b343b351311106dd58999c239420e7c7.js
@@ -0,0 +1,30 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "7b3d54aaad494df001ed76aa77712284",
+    "url": "/index.html"
+  },
+  {
+    "revision": "efb64395f9fac59e7b31",
+    "url": "/static/css/2.22b5b0f6.chunk.css"
+  },
+  {
+    "revision": "16d1d71ad3a3cd612ee3",
+    "url": "/static/css/main.8e19e90f.chunk.css"
+  },
+  {
+    "revision": "efb64395f9fac59e7b31",
+    "url": "/static/js/2.fe63b31d.chunk.js"
+  },
+  {
+    "revision": "86afaa925e0f432774bf95b19b6933fb",
+    "url": "/static/js/2.fe63b31d.chunk.js.LICENSE"
+  },
+  {
+    "revision": "16d1d71ad3a3cd612ee3",
+    "url": "/static/js/main.553a99c9.chunk.js"
+  },
+  {
+    "revision": "0e0de284a6b544b8ab0b",
+    "url": "/static/js/runtime-main.df9c3e05.js"
+  }
+]);
